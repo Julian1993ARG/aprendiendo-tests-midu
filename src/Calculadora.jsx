766a345b8834d756384316b5af0571ac1fd2c8f0
Julian <julianmartine@hotmail.com>
2022-10-07 14:56:58 -0300

    add new tests

--- src/Calculadora.jsx
+++ src/Calculadora.jsx
@@ -13,35 +13,52 @@ const equalSign = '='
 export const Calculadora = () => {
   const [value, setValue] = useState('')
 
-  const addOutputNumber = op => () => {
+  const addOutputNumber = op => {
+    console.log(op)
     setValue(value.toString().concat(op))
   }
-  const addOutputOperation = op => () => {
+  const addOutputOperation = op => {
     if (value.length) {
       const last = value.split('').pop()
       if (!isNaN(parseInt(last))) { setValue(value.concat(op)) }
     }
   }
-  const result = op => () => {
-    setValue(evaluate(op).toString())
+  const result = op => {
+    const last = value.split('').pop()
+    !isNaN(parseInt(last)) && setValue(evaluate(op).toString())
+  }
+  const onKeyDown = e => {
+    e.key === 'Enter' && result(value)
+    !isNaN(parseInt(e.key)) && addOutputNumber(e.key)
+
+    const simbol = operatios.find(operator => operator === e.key)
+    simbol && addOutputOperation(e.key)
+  }
+  const deleteLast = () => {
+    console.log('Aqui2')
+    if (value.length) {
+      console.log('Aqui')
+      setValue(value.substring(0, value.length - 1))
+    }
   }
 
   return (
     <div>
       <h1>Calculadora</h1>
-      <div role='grid' onKeyDown={(e) => setValue(e.key)}>
+      <div role='grid' onKeyDown={onKeyDown}>
         <input role='output' value={value} readOnly />
+        <button onClick={deleteLast}>←</button>
         {
           rows.map((row, inx) => (
             <div key={inx} role='row'>
-              {row.map(number => (<button onClick={addOutputNumber(number)} key={number}>{number}</button>))}
+              {row.map(number => (<button onClick={() => addOutputNumber(number)} key={number}>{number}</button>))}
             </div>
           ))
         }
-        {operatios.map((operation, i) => <button onClick={addOutputOperation(operation)} key={i}>{operation}</button>)}
+        {operatios.map((operation, i) => <button onClick={() => addOutputOperation(operation)} key={i}>{operation}</button>)}
         <button onClick={() => setValue('')}>C</button>
       </div>
-      <button onClick={result(value)}>{equalSign}</button>
+      <button onClick={() => result(value)}>{equalSign}</button>
     </div>
   )
 }
--- src/test/react.test.jsx
+++ src/test/react.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, cleanup, fireEvent } from '@testing-library/react'
-import { afterEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import { Calculadora } from '../Calculadora'
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
@@ -8,41 +8,46 @@ const operatios = ['-', '+', '/', '*']
 const equalSign = '='
 
 describe('Calculadora', () => {
+  beforeEach(() => { render(<Calculadora />) })
   afterEach(cleanup)// Cleanup cierra el render despues de cada test sino generaria una cola de renders
   it('Deberia renderizar el componente', () => {
-    render(<Calculadora />)
+    render()
   })
   it('deberia tener un titulo Calculadora', () => {
-    render(<Calculadora />)
+    render()
     screen.getByText('Calculadora')
   })
   it('Deberia renderizar los numeros del 0 al 9', () => {
-    render(<Calculadora />)
+    render()
     numbers.forEach(number => screen.getByText(number))
   })
   it('Deberia renderizar 4 filas con los numeros', () => {
-    render(<Calculadora />)
+    render()
     const rows = screen.getAllByRole('row')
     expect(rows.length).toBe(4)
   })
   it('Deberia renderizar todas las operaciones', () => {
-    render(<Calculadora />)
+    render()
     operatios.forEach(operation => screen.getByText(operation))
   })
   it('Deberia renderizar el simbolo = ', () => {
-    render(<Calculadora />)
+    render()
     screen.getByText('=')
   })
   it('Debe renderizar el boton C ', () => {
-    render(<Calculadora />)
+    render()
     screen.getByText('C')
   })
+  it('Debe renderizar ←', () => {
+    render()
+    screen.getByText('←')
+  })
   it('Deberia renderizar un imput', () => {
-    render(<Calculadora />)
+    render()
     screen.getByRole('output')
   })
   it('Deberia el output tener el value 1 cuando este se presione', () => {
-    render(<Calculadora />)
+    render()
 
     const one = screen.getByText('1')
     fireEvent.click(one)
@@ -50,7 +55,7 @@ describe('Calculadora', () => {
     expect(input.value).toBe('1')
   })
   it('Deberia el output tener el value de todos los numeros que se presionen', () => {
-    render(<Calculadora />)
+    render()
 
     const one = screen.getByText('1')
     fireEvent.click(one)
@@ -63,7 +68,7 @@ describe('Calculadora', () => {
     expect(input.value).toBe('123')
   })
   it('Deberia el output renderizar los simbolos', () => {
-    render(<Calculadora />)
+    render()
 
     const one = screen.getByText('1')
     fireEvent.click(one)
@@ -74,7 +79,7 @@ describe('Calculadora', () => {
     expect(input.value).toBe('1+1')
   })
   it('Deberia borrar el output al presionar C', () => {
-    render(<Calculadora />)
+    render()
     const setOutput = screen.getByText('C')
     const one = screen.getByText('1')
     const input = screen.getByRole('output')
@@ -82,8 +87,19 @@ describe('Calculadora', () => {
     fireEvent.click(setOutput)
     expect(input.value).toBe('')
   })
+  it('Deberia borrar el ultimo numero al presionar ←', () => {
+    render()
+    const one = screen.getByText('1')
+    const sum = screen.getByText('+')
+    const del = screen.getByText('←')
+    const input = screen.getByRole('output')
+    fireEvent.click(one)
+    fireEvent.click(sum)
+    fireEvent.click(del)
+    expect(input.value).toBe('1')
+  })
   it('Deberia devolver el resultado de la operacion', () => {
-    render(<Calculadora />)
+    render()
 
     const one = screen.getByText('1')
     fireEvent.click(one)
@@ -98,9 +114,22 @@ describe('Calculadora', () => {
 })
 
 describe('Test nivel medio', () => {
+  beforeEach(() => { render(<Calculadora />) })
   afterEach(cleanup)
+  it('No deberia permitir obtener el resultado si la operacion es incorrecta "1+ || 1*5+"', () => {
+    render()
+
+    const one = screen.getByText('1')
+    const sum = screen.getByText('+')
+    const resu = screen.getByText('=')
+    const input = screen.getByRole('output')
+    fireEvent.click(one)
+    fireEvent.click(sum)
+    fireEvent.click(resu)
+    expect(input.value).toBe('1+')
+  })
   it('No deberia permitir colocar un signo sino existe un numero', () => {
-    render(<Calculadora />)
+    render()
 
     const one = screen.getByText('1')
     const sum = screen.getByText('+')
@@ -110,7 +139,7 @@ describe('Test nivel medio', () => {
     expect(input.value).toBe('1')
   })
   it('No deberia permitir colocar varios signos de operaciones seguidos', () => {
-    render(<Calculadora />)
+    render()
 
     const one = screen.getByText('1')
     const sum = screen.getByText('+')
@@ -121,7 +150,7 @@ describe('Test nivel medio', () => {
     expect(input.value).toBe('1+')
   })
   it('Deberia poder hacer otro calculo con el resultado de una operacion', () => {
-    render(<Calculadora />)
+    render()
 
     const one = screen.getByText('1')
     const sum = screen.getByText('+')
@@ -136,10 +165,19 @@ describe('Test nivel medio', () => {
     expect(input.value).toBe('2+1')
   })
   it('Deberia renderizar las teclas presionadas en el output solo si son numeros o simbolos', () => {
-    render(<Calculadora />)
+    render()
     const input = screen.getByRole('output')
     fireEvent.focus(input)
-    // fireEvent.change(input, { target: { value: '5' } })
+    fireEvent.keyDown(input, {
+      charCode: 49,
+      code: 'Numpad1',
+      key: '1'
+    })
+    fireEvent.keyDown(input, {
+      charCode: 107,
+      code: 'NumpadAdd',
+      key: '+'
+    })
     fireEvent.keyDown(input, {
       charCode: 49,
       code: 'Numpad1',
@@ -150,20 +188,22 @@ describe('Test nivel medio', () => {
       code: 'KeyA',
       key: 'a'
     })
-    expect(input.value).toBe('1')
+    expect(input.value).toBe('1+1')
   })
-  // it('Al presionar enter debe mostrar el resultado', () => {
-  //   render(<Calculadora />)
-  //   const input = screen.getByRole('output')
-  //   fireEvent.focus(input)
-  //   // fireEvent.change(input, { target: { value: '5' } })
-  //   fireEvent.keyDown(input, {
-  //     charCode: 49,
-  //     code: 'Numpad1',
-  //     key: '1'
-  //   })
-  //   expect(input.value).toBe('1')
-  // })
-  // probando git amend
-  // intento de amend
+  it('Al presionar enter debe mostrar el resultado', () => {
+    render()
+
+    const one = screen.getByText('1')
+    const add = screen.getByText('+')
+    fireEvent.click(one)
+    fireEvent.click(add)
+    fireEvent.click(one)
+    const input = screen.getByRole('output')
+    fireEvent.keyDown(input, {
+      charCode: 13,
+      code: 'Enter',
+      key: 'Enter'
+    })
+    expect(input.value).toBe('2')
+  })
 })
